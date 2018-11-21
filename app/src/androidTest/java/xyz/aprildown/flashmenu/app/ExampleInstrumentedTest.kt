1dce786a9221c8df8ba9cc9281b19f139cf13f91
reed <deweyreed@protonmail.com>
2018-11-21 19:28:10 +0800

    Create project

---
+++ app/src/androidTest/java/xyz/aprildown/flashmenu/app/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package xyz.aprildown.flashmenu.app
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
+        assertEquals("xyz.aprildown.flashmenu.app", appContext.packageName)
+    }
+}
