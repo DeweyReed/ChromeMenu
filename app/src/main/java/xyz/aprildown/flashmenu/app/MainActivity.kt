a8558a3bd9a33a962084620e86c1a82a0396e71e
reed <deweyreed@protonmail.com>
2018-11-25 18:39:41 +0800

    Handle click events in onOptionsItemSelected

--- app/src/main/java/xyz/aprildown/flashmenu/app/MainActivity.kt
+++ app/src/main/java/xyz/aprildown/flashmenu/app/MainActivity.kt
@@ -1,6 +1,8 @@
 package xyz.aprildown.flashmenu.app
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import xyz.aprildown.flashmenu.AbstractAppMenuPropertiesDelegate
@@ -18,5 +20,10 @@ class MainActivity : AppCompatActivity() {
                 AppMenuHandler(this, AbstractAppMenuPropertiesDelegate(), R.menu.menu)
             )
         )
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        Toast.makeText(this, item?.title?.toString(), Toast.LENGTH_SHORT).show()
+        return true
     }
 }
