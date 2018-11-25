0d69d713b295d78781f3fe25cb3b951593f7566c
reed <deweyreed@protonmail.com>
2018-11-25 18:24:00 +0800

    Create AbstractAppMenuPropertiesDelegate

--- app/src/main/java/xyz/aprildown/flashmenu/app/MainActivity.kt
+++ app/src/main/java/xyz/aprildown/flashmenu/app/MainActivity.kt
@@ -1,16 +1,13 @@
 package xyz.aprildown.flashmenu.app
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import xyz.aprildown.flashmenu.AppMenu
+import xyz.aprildown.flashmenu.AbstractAppMenuPropertiesDelegate
 import xyz.aprildown.flashmenu.AppMenuButtonHelper
 import xyz.aprildown.flashmenu.AppMenuHandler
-import xyz.aprildown.flashmenu.AppMenuPropertiesDelegate
 
-class MainActivity : AppCompatActivity(), AppMenuPropertiesDelegate {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,34 +15,8 @@ class MainActivity : AppCompatActivity(), AppMenuPropertiesDelegate {
 
         textView.setOnTouchListener(
             AppMenuButtonHelper(
-                AppMenuHandler(this, this, R.menu.menu)
+                AppMenuHandler(this, AbstractAppMenuPropertiesDelegate(), R.menu.menu)
             )
         )
-    }
-
-    override fun shouldShowAppMenu(): Boolean = true
-
-    override fun prepareMenu(menu: Menu?) {
-
-    }
-
-    override fun getFooterResourceId(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getHeaderResourceId(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun shouldShowFooter(maxMenuHeight: Int): Boolean = false
-
-    override fun shouldShowHeader(maxMenuHeight: Int): Boolean = false
-
-    override fun onHeaderViewInflated(appMenu: AppMenu?, view: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onFooterViewInflated(appMenu: AppMenu?, view: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
---
+++ library/src/main/java/xyz/aprildown/flashmenu/AbstractAppMenuPropertiesDelegate.java
@@ -0,0 +1,50 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package xyz.aprildown.flashmenu;
+
+import android.view.Menu;
+import android.view.View;
+
+public class AbstractAppMenuPropertiesDelegate implements AppMenuPropertiesDelegate {
+    @Override
+    public boolean shouldShowAppMenu() {
+        return true;
+    }
+
+    @Override
+    public void prepareMenu(Menu menu) {
+
+    }
+
+    @Override
+    public int getFooterResourceId() {
+        return 0;
+    }
+
+    @Override
+    public int getHeaderResourceId() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldShowFooter(int maxMenuHeight) {
+        return false;
+    }
+
+    @Override
+    public boolean shouldShowHeader(int maxMenuHeight) {
+        return false;
+    }
+
+    @Override
+    public void onHeaderViewInflated(AppMenu appMenu, View view) {
+
+    }
+
+    @Override
+    public void onFooterViewInflated(AppMenu appMenu, View view) {
+
+    }
+}
